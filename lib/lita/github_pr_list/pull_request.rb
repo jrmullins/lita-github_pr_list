d8beb607bf2e508354578c98126f434cb3d2f43a
jrmullins <john.mullins@me.com>
2016-09-13 12:51:46 -0400

    less emoji's more authors

--- lib/lita/github_pr_list/pull_request.rb
+++ lib/lita/github_pr_list/pull_request.rb
@@ -39,7 +39,7 @@ module Lita
       def build_summary
         github_pull_requests.map do |pr_issue|
           status = repo_status("#{pr_issue.repository.full_name}", pr_issue)
-          "#{pr_issue.repository.name} #{status} #{pr_issue.title} #{pr_issue.pull_request.html_url}"
+          "#{pr_issue.repository.name} #{pr_issue.user.login} #{pr_issue.title} #{pr_issue.pull_request.html_url}"
         end
       end
 
